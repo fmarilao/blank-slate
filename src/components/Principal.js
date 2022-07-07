cc0b271237760ab91ac1a0c6751250152eeab2cb
Facundo <facundomarilao@gmail.com>
2022-07-07 16:35:56 -0300

    principal component and install react-modal

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,24 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Principal.css';
+import Modal from "react-modal";
+
+const avatars = [
+  "https://robohash.org/sitquibusdamautem.png?size=100x100&set=set1",
+  "https://robohash.org/assumendauttempore.png?size=100x100&set=set1",
+  "https://robohash.org/evenietquosvoluptatem.png?size=100x100&set=set1",
+  "https://robohash.org/suntnequefugiat.png?size=100x100&set=set1",
+  "https://robohash.org/quicorporistenetur.png?size=100x100&set=set1",
+  "https://robohash.org/repellatiustodolore.png?size=100x100&set=set1"
+];
 
 const Principal = () => {
-//
+  const [avatar, setAvatar] = useState(avatars[Math.floor(Math.random() * avatars.length)]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState();
+  const [gameId, setGameId] = useState();
+
+  const startGame = () => {};
+  const joinGame = () => {};
+
   return (
     <div className="App"> 
-        <div>
-            Avatar
-            Boton
+        <div className="Wrapper">
+          <img src={avatar} alt='avatar' onClick={() => setIsOpen(true)} />
+          <button type="button" onClick={() => setIsOpen(true)}>Cambiar</button>
         </div>
-        <div>
-            Texto
-            Input
-            Boton
+        <div className="Wrapper">
+            <h2>Usuario</h2>
+            <input
+              type="text"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => startGame()}
+            >
+              Nuevo Juego
+            </button>
         </div>
-        <div>
-            Texto
-            Input
-            Boton
+        <div className="Wrapper">
+        <h2>Game Id</h2>
+            <input
+              type="text"
+              value={gameId}
+              onChange={(e) => setGameId(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => joinGame()}
+            >
+              Unirse al Juego
+            </button>
         </div>        
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={()=> setIsOpen(false)}
+      >
+        <h1>Select your avatar</h1>
+        <div>
+          {avatars.map((avatarMap) => (
+            <img
+              className={avatarMap === avatar ? "Selected" : null}
+              src={avatarMap}
+              alt="avatar"
+              onClick={()=> {
+                setAvatar(avatarMap)
+              }}
+              />
+          ))}
+        </div>
+        <button 
+          type="button"
+          onClick={() => setIsOpen(false)}
+         >
+          Cancelar
+        </button>
+        <button 
+          type="button"
+          onClick={() => setIsOpen(false)}
+         >
+          Seleccionar
+        </button>
+      </Modal>
     </div>
   )
 }
