9a31e5fccfba07ed4db1b34b7bbc17aaa3ab904b
Facundo <facundomarilao@gmail.com>
2022-07-07 15:08:37 -0300

    create principal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import firebaseConfig from './firebaseConfig.json';
-
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { useEffect, useRef } from 'react';
+import Principal from './components/Principal';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -26,21 +24,8 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Principal />
     </div>
   );
 }
---
+++ src/components/Principal.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Principal.css';
+
+const Principal = () => {
+//
+  return (
+    <div className="App"> 
+        <div>
+            Avatar
+            Boton
+        </div>
+        <div>
+            Texto
+            Input
+            Boton
+        </div>
+        <div>
+            Texto
+            Input
+            Boton
+        </div>        
+    </div>
+  )
+}
+
+export default Principal
