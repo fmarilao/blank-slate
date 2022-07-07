e805733e5806a47184997a5098c8d94084bcf1f3
Juan Carlos del Valle <jc.ekinox@gmail.com>
2022-07-07 12:46:10 -0500

    integracion con firestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import firebaseConfig from './firebaseConfig.json';
+
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { useEffect, useRef } from 'react';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 function App() {
+  const subscription = useRef();
+  const subscribeToFirestore = async () => {
+    if (subscription.current) return;
+    const gamesDb = db.collection("games");
+    subscription.current = gamesDb.doc("D7DGcWqHk173BDCOdmnD").onSnapshot((doc) => {
+      console.log(doc.data());
+    });
+  }
+
+  useEffect(() => {
+    subscribeToFirestore();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
