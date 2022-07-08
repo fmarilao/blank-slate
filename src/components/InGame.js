34af91ce753097e99d59b64f2c41897aeb54e4a6
Facundo <facundomarilao@gmail.com>
2022-07-08 11:01:20 -0300

    ingame functions, componente turno, agregado estado en player context

--- src/components/InGame.js
+++ src/components/InGame.js
@@ -1,17 +1,58 @@
 import React, { useContext } from 'react';
 import FirestoreContext from '../context/FirestoreContext';
+import PlayerContext from '../context/PlayerContext';
 import './InGame.css';
 import Lobby from './Lobby';
 import Turno from './Turno';
+import firebase from "firebase/app";
 
 const InGame = () => {
     const { db, joinedGameId, gameData } = useContext(FirestoreContext);
+    const { username, selectedWord} = useContext(PlayerContext);
 
+    const currRound = gameData.rounds.length;
+    const words = ['___botellas', 'abre_____', 'en_________', 'porta_____',  '_____magnetismo']
+    
+    //guardamos en selectedwords lo que nos devuelve firestore con el array de usuarios y sus palabras elegidas
+    const selectedWords = gameData[`selectedWords_${currRound}`]
+
+    // verificamos en el array de palabras elegidas si yo ya elegi o no
+    // para habilitar o deshabilitar el boton de enviar
+    const sent = selectedWord.find((w) => w.username === username);
+
+    //iniciamos el juego por el creador
     const startGame = async () => {
-        await db.collection("games").doc(joinedGameId).update({started: true});
+        if (currRound === words.length) {
+            await db.collection("games")
+            .doc(joinedGameId)
+            .update({
+              ended: true,
+            })
+        } else {
+            const guessWord = words[currRound]
+            await db.collection("games").doc(joinedGameId).update({
+                started: true,
+                rounds: firebase.firestore.FieldValue.arrayUnion(guessWord)            
+            });
+        }
     }
+
+    const sendWord = async () => {
+        // si no envio aun la palabra dejamos enviar
+        if (!sent) {
+            // actualizamos en firestore un array llamado selectedWords_##nroderound con el username y la palabra elegida
+            await db.collection("games").doc(joinedGameId).update({
+              [`selectedWords_${currRound}`]: firebase.firestore.FieldValue.arrayUnion({
+                username: username,
+                selectedWord,
+              })            
+            });
+        }
+    }
+
     if (gameData.started) { 
-        return <Turno />;
+        const currWord = gameData.rounds[gameData.rounds.length - 1];
+        return <Turno id={joinedGameId} players={gameData.players} guessWord={currWord} sent={!!sent} onUpdate={sendWord} selected={selectedWords} />;
     }
     return <Lobby id={joinedGameId} players={gameData.players} onStart={startGame} />;
 }
--- src/components/Turno.js
+++ src/components/Turno.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import PlayerContext from "../context/PlayerContext";
 import "./Turno.css";
 
-const Turno = ({ id, players }) => {
-  //
+const Turno = ({ id, players, guessWord, sent, onUpdate, selected }) => {
+  const { selectedWord, setSelectedWord } = useContext(PlayerContext)
+
+  const renderPlayerStatus = () => {
+    return players.map((p) => {
+      // recorre todos los players
+      // verificamos si el player ya mando la palabra
+      // segun el array de selectedwords
+      const sent = selected.find((w)=> w.username === p.username)
+      return (
+        <div>{p.username} {sent ? "OK" : "Pending"} </div>
+      )
+    })
+  }
+
   return (
     <div className="Main">
       <h2>Turno</h2>
-    </div>
+      <div>
+        <h3>game id:</h3>
+        <p>{id}</p>
+      </div>
+      <div>
+        <h3>online players:</h3>
+        <p>{players.length}</p>
+      </div>
+      <div>
+        la palabra a completar es: {guessWord}
+      </div>
+     {!sent ? (
+      <div>
+        <input
+          type="text"
+          value={selectedWord}
+          onChange={(e) => setSelectedWord}
+        />
+        <button
+          type="button"
+          onClick={onUpdate}
+          >
+          Enviar  
+          </button>
+      </div>
+     ) : renderPlayerStatus()}
+  </div>
   );
 };
 
